4686c4916b024105025f9ae51d2115adf03317db
Jesus <jeshua0423@gmail.com>
2024-10-17 23:09:05 -0600

    Cambios agregar buscador y mas animaciones con tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Gallery } from './components/Gallery';
 
 function App() {
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-orange-400 pb-6">
-        <h1 className="text-5xl font-bold text-center mb-10 pt-9">
-          Dragon Ball
-        </h1>
-        <Gallery />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center min-h-screen bg-orange-400 pb-6">
+                <h1 className="text-5xl font-bold text-center mb-10 pt-9">
+                    Dragon Ball
+                </h1>
+                <Gallery />
+            </div>
+        </>
+    );
 }
 
 export default App;
